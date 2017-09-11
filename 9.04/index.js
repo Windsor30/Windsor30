91e35d14e78c2714976e093795ff82f0be735feb
Windsor30 <904402987@qq.com>
2017-09-11 08:56:44 +0800

    exercise

---
+++ 9.04/index.js
@@ -0,0 +1,13 @@
+/*
+* @Author: Windsor
+* @Date:   2017-09-04 11:03:32
+* @Last Modified by:   Windsor
+* @Last Modified time: 2017-09-04 11:20:22
+*/
+alert('这是一个外部的js文件');
+// alert(111);
+// alert("张三");
+// console.log(111111);//往控制台输出
+console.log('这香浓的诱惑');//往控制台输出
+// document.write(112233);
+// document.write('<h1>我轻轻的尝一口你说的爱我</h1>');
